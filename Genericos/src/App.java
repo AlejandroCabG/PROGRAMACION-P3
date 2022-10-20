bae7b8f56ca5eb767f05b6cb0a817ec6591eef56
AlejandroCabG <acabrerag@campus.eug.es>
2022-10-21 00:34:52 +0200

    Commit
    Primer commit

---
+++ Genericos/src/App.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) {
+        //Creo un objeto llamado stack para acceder a la clase Stack
+        Stack stack = new Stack();
+
+        System.out.println(stack.isEmpty());
+       // stack.add("item");
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.removeFirst());
+        System.out.println(stack.getFirst());
+        System.out.println(stack.aniadir("Cocodrilos"));
+        System.out.println(stack.lista);
+        System.out.println(stack.toString());
+
+
+    }
+}
---
+++ Genericos/src/Stack.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+public class Stack<T> {
+    LinkedList<T> lista = new LinkedList<T>();
+
+    //Constructor
+    public Stack() {
+        super();
+    }
+
+    public boolean isEmpty() {
+        return lista.isEmpty();
+    }
+
+    public T removeFirst() {
+        return lista.removeFirst();
+    }
+    public T getFirst() {
+        return lista.getFirst();
+    }
+    public void add(T item){
+        lista.add((T) "Perros");
+        lista.add((T) "Gatos");
+        System.out.println(lista);
+    }
+    public T aniadir(T item) {
+        lista.addFirst(item);
+        return item;
+    }
+
+    public String toString() {
+        return lista.toString();
+    }
+
+
+}
+
